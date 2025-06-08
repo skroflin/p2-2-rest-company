142fb8fd581a36a6949926acf4004aea895fb3d6
Sven Kroflin (jer je profesor prigovarao na imena) <sven.kroflin@gmail.com>
2025-06-08 20:20:27 +0200

    Kreirane prve rute za OdjelController
    Kreirane prve rute za OdjelController -get, getBySifra i post. Usput dodana dokumentacija za pojedinačne endpoint-ove.

--- src/main/java/ffos/skroflin/controller/OdjelController.java
+++ src/main/java/ffos/skroflin/controller/OdjelController.java
@@ -4,10 +4,130 @@
  */
 package ffos.skroflin.controller;
 
+import ffos.skroflin.model.Odjel;
+import ffos.skroflin.model.dto.OdjelDTO;
+import ffos.skroflin.service.OdjelService;
+import ffos.skroflin.service.TvrtkaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author svenk
  */
+
+@Tag(name = "Odjel", description = "Dostupne rute za entitet tvrtka. Sve CRUD funkcionalnosti - GET, POST, PUT i DELETE.")
+@RestController
+@RequestMapping("/api/skroflin/odjel")
 public class OdjelController {
+    private final OdjelService odjelService;
+    private final TvrtkaService tvrtkaService;
+
+    public OdjelController(OdjelService odjelService, TvrtkaService tvrtkaService) {
+        this.odjelService = odjelService;
+        this.tvrtkaService = tvrtkaService;
+    }
     
+    @Operation(
+            summary = "Dohvaća sve odjele", tags = {"get", "odječ"},
+            description = "Dohvaća sve odjele sa svim podacima"
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Odjel.class)))),
+                @ApiResponse(responseCode = "500", description = "Interna pogreška servera", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html"))
+            })
+    @GetMapping("/get")
+    public ResponseEntity get(){
+        try {
+            return new ResponseEntity<>(odjelService.getAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @Operation(
+            summary = "Dohvaća odjel po šifri",
+            description = "Dohvaća odjel po danoj šifri sa svim podacima. "
+            + "Ukoliko ne postoji odjel za danu šifru vraća prazan odgovor",
+            tags = {"odjel", "getBy"},
+            parameters = {
+                @Parameter(
+                        name = "sifra",
+                        allowEmptyValue = false,
+                        required = true,
+                        description = "Primarni ključ odjela u bazi podataka, mora biti veći od nula",
+                        example = "2"
+                )})
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Odjel.class), mediaType = "application/json")),
+        @ApiResponse(responseCode = "204", description = "Ne postoji odjel za danu šifru", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html")),
+        @ApiResponse(responseCode = "400", description = "Šifra mora biti veća od nula", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html")),
+        @ApiResponse(responseCode = "500", description = "Interna pogreška servera", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html"))
+    })
+    @GetMapping("/getBySifra")
+    public ResponseEntity getBySifra(
+            @RequestParam int sifra
+    ){
+        try {
+            if (sifra <= 0) {
+                return new ResponseEntity<>("Šifra mora biti veća od 0" + " " + sifra, HttpStatus.BAD_REQUEST);
+            }
+            
+            Odjel o = odjelService.getBySifra(sifra);
+            if (o == null) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            
+            return new ResponseEntity<>(o, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @Operation(
+            summary = "Kreira novi odjel",
+            tags = {"post", "odjel"},
+            description = "Kreira novi odjel. Naziv i lokacija odjela obavezno!")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Kreirano", content = @Content(schema = @Schema(implementation = Odjel.class), mediaType = "application/json")),
+        @ApiResponse(responseCode = "400", description = "Loš zahtjev (nije primljen dto objekt ili ne postoji naziv ili lokacaij)", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html")),
+        @ApiResponse(responseCode = "500", description = "Interna pogreška servera", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html"))
+    })
+    @PostMapping("/post")
+    public ResponseEntity post(
+            @RequestBody(required = true) OdjelDTO dto
+    ){
+        try {
+            if (dto == null) {
+                return new ResponseEntity<>("Podaci nisu primljeni" + " " + dto, HttpStatus.BAD_REQUEST);
+            }
+            
+            if (dto.naziv() == null || dto.naziv().isEmpty()) {
+                return new ResponseEntity<>("Naziv odjela je obavezan!" + dto.naziv(), HttpStatus.BAD_REQUEST);
+            }
+            
+            if (dto.lokacija() == null || dto.lokacija().isEmpty()) {
+                return new ResponseEntity<>("Lokacija odjela je obavezna!" + dto.lokacija(), HttpStatus.BAD_REQUEST);
+            }
+            
+            return new ResponseEntity<>(odjelService.post(dto), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- src/main/java/ffos/skroflin/controller/TvrtkaController.java
+++ src/main/java/ffos/skroflin/controller/TvrtkaController.java
@@ -7,7 +7,6 @@ package ffos.skroflin.controller;
 import ffos.skroflin.model.Odjel;
 import ffos.skroflin.model.Tvrtka;
 import ffos.skroflin.model.dto.TvrtkaDTO;
-import ffos.skroflin.service.OdjelService;
 import ffos.skroflin.service.TvrtkaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
