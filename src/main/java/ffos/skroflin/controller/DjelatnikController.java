7f92370407b7d955b925075327fc42a2c8391bc1
Sven Kroflin (jer je profesor prigovarao na imena) <sven.kroflin@gmail.com>
2025-06-09 09:43:46 +0200

    Dodani get i getBySifra
    Dodani prvi endpoint-ovi u DjelatnikController -get i getBySifra. Dodana i dokumentacija također.

--- src/main/java/ffos/skroflin/controller/DjelatnikController.java
+++ src/main/java/ffos/skroflin/controller/DjelatnikController.java
@@ -4,10 +4,95 @@
  */
 package ffos.skroflin.controller;
 
+import ffos.skroflin.model.Djelatnik;
+import ffos.skroflin.service.DjelatnikService;
+import ffos.skroflin.service.OdjelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author svenk
  */
+
+@Tag(name = "Djelatnik", description = "Dostupne rute za entitet tvrtka. Sve CRUD funkcionalnosti - GET, POST, PUT i DELETE.")
+@RestController
+@RequestMapping("/api/skroflin/djelatnik")
 public class DjelatnikController {
+    private final DjelatnikService djelatnikService;
+    private final OdjelService odjelService;
+
+    public DjelatnikController(DjelatnikService djelatnikService, OdjelService odjelService) {
+        this.djelatnikService = djelatnikService;
+        this.odjelService = odjelService;
+    }
     
+    @Operation(
+            summary = "Dohvaća sve djelatnike", tags = {"get", "djelatnik"},
+            description = "Dohvaća sve djelatnike sa svim svojim pripadajućim podacima."
+    )
+    @ApiResponses(
+            value = {
+                @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Djelatnik.class)))),
+                @ApiResponse(responseCode = "500", description = "Interna pogreška servera", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html"))
+            })
+    @GetMapping("/get")
+    public ResponseEntity get(){
+        try {
+            return new ResponseEntity<>(djelatnikService.getAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @Operation(
+            summary = "Dohvaća djelatnika po šifri",
+            description = "Dohvaća djelatnika po danoj šifri sa svim svojim pripadajućim podacima. "
+            + "Ukoliko ne postoji djelatnik za danu šifru vraća prazan odgovor",
+            tags = {"djelatnik", "getBy"},
+            parameters = {
+                @Parameter(
+                        name = "sifra",
+                        allowEmptyValue = false,
+                        required = true,
+                        description = "Primarni ključ djelatnika u bazi podataka, mora biti veći od nula",
+                        example = "2"
+                )})
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Djelatnik.class), mediaType = "application/json")),
+        @ApiResponse(responseCode = "204", description = "Ne postoji profesor za danu šifru", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html")),
+        @ApiResponse(responseCode = "400", description = "Šifra mora biti veća od nula", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html")),
+        @ApiResponse(responseCode = "500", description = "Interna pogreška servera", content = @Content(schema = @Schema(implementation = String.class), mediaType = "text/html"))
+    })
+    @GetMapping("/getBySifra")
+    public ResponseEntity getBySifra(
+            @RequestParam int sifra
+    ){
+        try {
+            if (sifra <= 0) {
+                return new ResponseEntity<>("Šifra mora biti veća od nule:" + " " + sifra, HttpStatus.BAD_REQUEST);
+            }
+            
+            Djelatnik d = djelatnikService.getBySifra(sifra);
+            if (d == null) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            
+            return new ResponseEntity<>(d, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
